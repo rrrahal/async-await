c0e5462c73d91560748be7dd49e8cf72b2457043
Rafael Rahal <rrrahal@gmail.com>
2018-07-06 12:04:58 -0300

    add a way to handle errros with async await

--- currency-convert.js
+++ currency-convert.js
@@ -3,17 +3,30 @@
 const axios = require('axios');
 
 const getExchangeRate = async (from, to) => {
-	const response = await axios.get(
-		'http://data.fixer.io/api/latest?access_key=bdbfc53af7612742e45b593edda74a82&format=1'
-	);
-	const euro = 1 / response.data.rates[from];
-	const rate = euro * response.data.rates[to];
-	return rate;
+	try {
+		const response = await axios.get(
+			'http://data.fixer.io/api/latest?access_key=bdbfc53af7612742e45b593edda74a82&format=1'
+		);
+		const euro = 1 / response.data.rates[from];
+		const rate = euro * response.data.rates[to];
+
+		if (isNaN(rate)) {
+			throw new Error();
+		}
+
+		return rate;
+	} catch (e) {
+		throw new Error(`Unable to get exchange rate for ${from} and ${to}`);
+	}
 };
 
 const getCountries = async (currencyCode) => {
-	const response = await axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`);
-	return response.data.map((country) => country.name);
+	try {
+		const response = await axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`);
+		return response.data.map((country) => country.name);
+	} catch (e) {
+		throw new Error(`Unable to get countries that use ${currencyCode}`);
+	}
 };
 
 const convertCurrency = (from, to, amount) => {
@@ -39,6 +52,29 @@ const convertCurrencyAlt = async (from, to, amount) => {
 	)}`;
 };
 
-convertCurrencyAlt('USD', 'CAD', 20).then((message) => {
-	console.log(message);
-});
+convertCurrencyAlt('USD', 'CAD', 20)
+	.then((message) => {
+		console.log(message);
+	})
+	.catch((e) => {
+		console.log(e.message);
+	});
+
+const add = async (a, b) => a + b;
+
+const doWork = async () => {
+	try {
+		const result = await add(12, 13);
+		return result;
+	} catch (e) {
+		return 10;
+	}
+};
+
+doWork()
+	.then((data) => {
+		console.log(data);
+	})
+	.catch((e) => {
+		console.log('something went wrong', e);
+	});
